e9ea7b98283730268ab0de18053bcc545b3779e2
Muhammad Faizan <faizaniqbal2911@gmail.com>
2026-07-09 02:20:26 +0500

    fix api baseURL missing /api prefix

--- client/src/service/api.js
+++ client/src/service/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 //create a axios instance with base configuration
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL,
+  baseURL: `${process.env.REACT_APP_API_URL}/api`,
   headers: { "Content-Type": "application/json" },
 });
 
